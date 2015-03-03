a591cceddf0782124ec10aa72fd15e370fcea8bb
Lee Zamparo <zamparo@gmail.com>
2015-03-03 12:16:09 -0500

    fixed init of split-merge model, driver scripts for dpmm sampling

--- dpmm/time_dpmm.py
+++ dpmm/time_dpmm.py
@@ -47,11 +47,11 @@ for arg in vars(args):
         print arg, ": ", getattr(args, arg)
 
 if args.algorithm == 'gibbs':
-        dpmm = DPMM(n_components=n_components.astype(int),alpha=pre_alpha,do_sample_alpha=True)
+        dpmm = DPMM(n_components=n_components.astype(int),alpha=pre_alpha,do_sample_alpha=True,do_init=True)
         dpmm.fit_collapsed_Gibbs(X,do_sample_alpha=True,do_kmeans=True,max_iter=args.num_itns)
 elif args.algorithm == 'split_merge':
-        dpmm = DPMM(n_components=-1,alpha=pre_alpha,do_sample_alpha=True)
-        dpmm.fit_conjugate_split_merge(X,do_sample_alpha=True,do_kmeans=False,max_iter=args.num_itns)
+        dpmm = DPMM(n_components=1,alpha=pre_alpha,do_sample_alpha=True)
+        dpmm.fit_conjugate_split_merge(X,do_sample_alpha=True,do_kmeans=False,max_iter=args.num_itns,do_init=True)
 
 # save the dpmm to outfile
 pickle.dump(dpmm, open( args.save, "wb" ) )
